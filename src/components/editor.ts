b613ae164603f4230451cb94447bec8169975726
Jesper <jjensenmoeller@gmail.com>
2021-11-19 13:34:18 +0100

    editable

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -3,9 +3,21 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("rte-editor")
 export class RTEEditor extends LitElement {
-  static styles = [css``];
+  static styles = [
+    css`
+      #editor {
+        width: 100%;
+        min-height: 200px;
+        border: 1px solid black;
+      }
+    `,
+  ];
 
   render() {
-    return html`<h1>I Am The Editor</h1>`;
+    return html`<div id="editor" contenteditable="true">
+      <h1>Header</h1>
+      <code>Code goes here</code>
+      <b>Bold text</b>
+    </div>`;
   }
 }
