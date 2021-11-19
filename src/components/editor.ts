5dec429016cd0edf4d94e4acb4b0db068175b864
Jesper <jjensenmoeller@gmail.com>
2021-11-19 13:46:38 +0100

    stuff

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -15,9 +15,19 @@ export class RTEEditor extends LitElement {
 
   render() {
     return html`<div id="editor" contenteditable="true">
-      <h1>Header</h1>
-      <code>Code goes here</code>
-      <b>Bold text</b>
+      <h1>Header1</h1>
+      <h2>Header2</h2>
+      <h3>Header3</h3>
+      <div>Normal Text</div>
+      <code>Code</code> <br />
+      <b>Bold text</b> <br />
+      <ul>
+        <li>Bullet</li>
+        <li>List</li>
+      </ul>
+      <br />
+      <img src="" alt="I am image" />
+      <div>Custom Elements</div>
     </div>`;
   }
 }
