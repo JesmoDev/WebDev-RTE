ad2a3ed19bd3d0aee6f83413f192a8b6b7d81339
Jesper <jjensenmoeller@gmail.com>
2021-12-03 03:57:39 +0100

    slate integration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
+import { createEditor } from "slate";
 import "./components/editor";
 
 @customElement("app-element")
@@ -12,7 +13,25 @@ export class App extends LitElement {
     `,
   ];
 
+  @state()
+  value = [
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ];
+
+  handleChange(event) {
+    console.log(event);
+  }
+
+  private editor = createEditor();
+
   render() {
-    return html`<rte-editor></rte-editor>`;
+    return html`<rte-editor
+      .editor=${this.editor}
+      .value=${this.value}
+      @change=${this.handleChange}
+    ></rte-editor>`;
   }
 }
--- src/components/editor.ts
+++ src/components/editor.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { BaseEditor, createEditor, Editor, Transforms } from "slate";
 
 @customElement("rte-editor")
 export class RTEEditor extends LitElement {
@@ -12,26 +13,55 @@ export class RTEEditor extends LitElement {
       }
     `,
   ];
+  private readonly emptyValue = [{ children: [{ text: "asf" }] }];
+
+  private _editor: BaseEditor = createEditor();
+
+  @property()
+  value;
+
+  constructor() {
+    super();
+    this._editor.children = this.emptyValue;
+    this._editor.selection = {
+      anchor: { path: [0, 0], offset: 0 },
+      focus: { path: [0, 0], offset: 15 },
+    };
+  }
+
+  start = {
+    path: [0, 0],
+    offset: 0,
+  };
+
+  onKeyDown(event: KeyboardEvent) {
+    event.preventDefault();
+  }
+
+  onInput(event: InputEvent) {
+    if (event.data) {
+      this._editor.insertText(event.data);
+    }
+    this.requestUpdate();
+  }
+
+  blockSelector(block) {
+    console.log("hallo", block);
+
+    switch (block.type) {
+      case "paragraph":
+        return html`<p>${block.children[0].text}</p>`;
+
+      default:
+        return html`${block.children[0].text}`;
+    }
+  }
 
   render() {
-    return html`<div id="editor" contenteditable="true">
-      <h1>Header1</h1>
-      <h2>Header2</h2>
-      <h3>Header3</h3>
-      <div>Normal Text</div>
-      <code>Code</code> <br />
-      <b>Bold text</b> <br />
-      <ol>
-        <li>Ordered</li>
-        <li>List</li>
-      </ol>
-      <ul>
-        <li>Bullet</li>
-        <li>List</li>
-      </ul>
-      <br />
-      <img src="" alt="I am image" />
-      <div>Custom Elements</div>
+    return html`<div id="editor" contenteditable="true" @input=${this.onInput}>
+      ${this._editor.children.map((block) => {
+        return this.blockSelector(block);
+      })}
     </div>`;
   }
 }
