f2b58c9b9028b9413dc75db367f9e3217fe844cb
predeezRav <prat0025@edu.ucl.dk>
2022-01-18 21:06:37 +0100

    Finished adding block and images

--- src/components/tutorialPanel.element.ts
+++ src/components/tutorialPanel.element.ts
@@ -23,14 +23,53 @@ export class TutorialPanelElement extends LitElement {
         height: 600px;
         display: flex;
         flex-direction: column;
+
+        box-shadow: 2px 2px 5px #a4a2a2;
       }
 
       #how-to-start {
         text-align: center;
       }
 
-      #get-started-shortcut {
-        font-size: 2em;
+      #how-to-h1 {
+        text-align: center;
+        font-style: italic;
+        font-weight: 400;
+      }
+
+      #how-to-h2 {
+        text-align: center;
+        font-weight: 300;
+      }
+
+      #how-to-h3 {
+        text-align: center;
+        font-weight: 300;
+      }
+
+      #how-to-start {
+      }
+      #how-to-img {
+        width: 35%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      #how-to-img-2 {
+        width: 90%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      #how-to-hover {
+        text-align: center;
+        padding-top: 10%;
+      }
+      #how-to-hover-para {
+        text-align: center;
+        font-style: italic;
       }
 
       #element {
@@ -55,7 +94,14 @@ export class TutorialPanelElement extends LitElement {
   protected render(): TemplateResult {
     return html`<div id="tutorial-menu">
       <div id="how-to">
-        <p id="how-to-start">Shortcuts</p>
+        <h1 id="how-to-h1">HOW TO GET STARTED</h1>
+        <h2 id="how-to-h2">"Open Editor"</h2>
+        <h2 id="how-to-h3">ctrl+k</h2>
+        <p id="how-to-start">Select a block to build with</p>
+        <img id="how-to-img" src="src/img/icons/blockMenu.png" alt="" />
+        <p id="how-to-hover">Highlight text to edit</p>
+        <img id="how-to-img-2" src="src/img/icons/hoverMenu.png" alt="" />
+        <p id="how-to-hover-para">(Or use shortcuts)</p>
       </div>
     </div>`;
   }
