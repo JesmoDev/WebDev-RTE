fc82a590d9892b7cb2cd19d73298301fad084d0d
Jesper <jjensenmoeller@gmail.com>
2022-01-21 11:38:37 +0100

    prebundle dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,6 +27,15 @@ module.exports = {
       config.optimizeDeps.include.push('lit/directives/style-map.js');
       config.optimizeDeps.include.push('lit/directives/if-defined.js');
       config.optimizeDeps.include.push('lit/directives/unsafe-html.js');
+      config.optimizeDeps.include.push('@umbraco-ui/uui-base/lib/registration');
+      config.optimizeDeps.include.push('lit/directives/repeat.js');
+      config.optimizeDeps.include.push('@tiptap/core');
+      config.optimizeDeps.include.push('@tiptap/starter-kit');
+      config.optimizeDeps.include.push('@tiptap/extension-underline');
+      config.optimizeDeps.include.push('@tiptap/extension-text-align');
+      config.optimizeDeps.include.push('@tiptap/extension-image');
+      config.optimizeDeps.include.push('@tiptap/extension-link');
+      config.optimizeDeps.include.push('@tiptap/extension-bubble-menu');
     }
 
     return config;
